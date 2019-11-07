ebaa8b54177ada5fb29b7039eab67d74bc3a5136
RUIYANG ZHANG <zhangruiyang36@gmail.com>
2019-11-07 16:08:00 +0800

    change to convert func

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.13
 
 require (
 	github.com/stretchr/testify v1.4.0 // indirect
-	github.com/tidwall/gjson v1.3.4 // indirect
+	github.com/tidwall/gjson v1.3.4
 	github.com/tidwall/sjson v1.0.4
 )
--- setter.go
+++ setter.go
@@ -3,17 +3,17 @@ package jsonpath
 import (
 	"encoding/json"
 	"github.com/tidwall/sjson"
+	"github.com/tidwall/gjson"
 )
 
-func Set(jsonString string, jsonPath string, target string) (result string){
+func Set(jsonString string, jsonPath string, convertFunc func(input string) string) (result string){
 	var j interface{}
 	json.Unmarshal([]byte(jsonString), &j)
 	_, arr,_ := JsonPathLookup(j, jsonPath)
 
 	for _,jPath:=range arr{
-		jsonString,_ = sjson.Set(jsonString, jPath, target)
+		jsonString,_ = sjson.Set(jsonString, jPath, convertFunc(gjson.Get(jsonString, jPath).String()))
 	}
-
 	result = jsonString
 	return
 }
